143542d4579bb822622ea16db0be5ff0652b8d1d
Adrian De León <adriandleon@gmail.com>
2017-08-04 08:14:51 -0400

    Constantes y Operadores
    Video 7

---
+++ src/Constantes_Operadores.java
@@ -0,0 +1,15 @@
+
+public class Constantes_Operadores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float a = 5;
+		float b;
+		b = 7;
+		float c = b / a;
+//		c -= 6;
+		
+		System.out.println(c);
+	}
+
+}
