b9333c845ed13802f84c86cbcbfcc817a18125b1
Adrian De León <adriandleon@gmail.com>
2017-08-06 21:39:06 -0400

    Constantes y Operadores II
    Video 8

--- src/Constantes_Operadores.java
+++ src/Constantes_Operadores.java
@@ -3,13 +3,22 @@ public class Constantes_Operadores {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		float a = 5;
-		float b;
-		b = 7;
-		float c = b / a;
-//		c -= 6;
 		
-		System.out.println(c);
+		final double apulgadas = 2.54;
+		
+		double cm = 6;
+		double resultado = cm / apulgadas;
+		
+		System.out.println("En " + cm + "cm hay " + resultado + " pulgadas.");
+		
+		// Declaración de variables en una misma línea
+		int ope1, ope2, result;
+		
+		ope1 = 8;
+		ope2 = 7;
+		result = ope1 + ope2;
+		
+		System.out.println(result);
 	}
 
 }
