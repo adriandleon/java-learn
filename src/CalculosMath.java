ea074607e98acae49063e4adafbea7d3ca9d7bc9
Adrian De León <adriandleon@gmail.com>
2017-08-06 23:26:38 -0400

    Clase Math
    Video 9

---
+++ src/CalculosMath.java
@@ -0,0 +1,13 @@
+
+public class CalculosMath {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double raiz = Math.sqrt(9);
+		
+		System.out.println(raiz);
+		
+	}
+
+}
