ae7f3fdfcac382ce34651a95160296b6be6f11ae
Adrian De León <adriandleon@gmail.com>
2017-08-08 09:41:41 -0400

    Clase Math II
    Video 10

--- src/CalculosMath.java
+++ src/CalculosMath.java
@@ -4,9 +4,15 @@ public class CalculosMath {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		double raiz = Math.sqrt(9);
+//		double raiz = Math.sqrt(9);
 		
-		System.out.println(raiz);
+		double base = 5;
+		double exponente = 3;
+		
+		int resultado = (int)Math.pow(base, exponente);
+		
+		System.out.println("El resultado de " + base + " elevado a " 
+				+ exponente + " es " + resultado);
 		
 	}
 
