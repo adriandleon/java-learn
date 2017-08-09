e53e3cbc28efb1aa9444311969e00ffb49183754
Adrian De León <adriandleon@gmail.com>
2017-08-08 22:36:05 -0400

    Entrada Salida datos I
    Video 14

---
+++ src/EntradaEjemplo1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class EntradaEjemplo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String nombreUsuario;
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Introduce tu nombre, por favor");
+		
+		nombreUsuario = entrada.nextLine();
+		
+		System.out.println("Introduce tu edad, por favor");
+		
+		int edad = entrada.nextInt();
+		
+		System.out.println("Hola " + nombreUsuario + ". El año que viene " +
+		"tendrás " + (edad + 1) + " años");
+	}
+
+}
