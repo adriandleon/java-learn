929069e6a5f61f9cb97df83083b9c189adaeff37
Adrian De León <adriandleon@gmail.com>
2017-08-08 21:38:00 -0400

    Clase String II
    Video 12

--- src/ManipulaCadenas.java
+++ src/ManipulaCadenas.java
@@ -8,13 +8,13 @@ public class ManipulaCadenas {
 		
 		System.out.println("Mi nombre es " + nombre);
 		
-		System.out.println("Mi nombre tiene " + nombre.length() + " carácteres");
+		System.out.println("Mi nombre tiene " + nombre.length() + " carÃ¡cteres");
 		
 		System.out.println("La primera letra de mi nombre es " + nombre.charAt(0));
 		
 		int ultima_letra = nombre.length();
 			
-		System.out.println("La última letra es " + nombre.charAt(ultima_letra-1));
+		System.out.println("La Ãºltima letra es " + nombre.charAt(ultima_letra-1));
 		
 	}
 
