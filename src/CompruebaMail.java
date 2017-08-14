259642d316b71fb65ffb7a9ab45c4ced4304fa95
Adrian De León <adriandleon@gmail.com>
2017-08-14 08:46:38 -0400

    Bucles V
    Video 22

--- src/CompruebaMail.java
+++ src/CompruebaMail.java
@@ -5,21 +5,25 @@ public class CompruebaMail {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		boolean arroba = false;
+		int arroba = 0;
+		boolean punto = false;
 		String email = JOptionPane.showInputDialog("Introduce email");
 		
 		for (int i=0; i < email.length(); i++) {
 			if (email.charAt(i) == '@') {
-				arroba = true;
+				arroba++;
+			}
+			
+			if (email.charAt(i) == '.') {
+				punto = true;
 			}
 		}
 		
-		if (arroba) {
+		if (arroba == 1 && punto) {
 			System.out.println("Es correcto");
 		}
 		else {
-			System.out.println("Email no válido");
+			System.out.println("Email no vÃ¡lido");
 		}
 	}
-
 }
