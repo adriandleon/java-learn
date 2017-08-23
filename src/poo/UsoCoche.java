87a92c7f45b45e1c262c60bada61266183190cd0
Adrian De León <adriandleon@gmail.com>
2017-08-23 08:50:40 -0400

    POO II. Metodo Constructor
    Video 28

---
+++ src/poo/UsoCoche.java
@@ -0,0 +1,13 @@
+package poo;
+
+public class UsoCoche {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Coche Renault = new Coche(); // Instanciar una clase
+		
+		System.out.println("Este coche tiene " + Renault.ruedas + " ruedas.");
+	}
+
+}
