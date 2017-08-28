f04d535d0416f9f5a371bd6bb3a20c67c60a0474
Adrian De León <adriandleon@gmail.com>
2017-08-28 08:50:07 -0400

    POO VII. Construccion objetos II
    Video 33

---
+++ src/poo/UsoEmpleado.java
@@ -0,0 +1,24 @@
+package poo;
+
+import java.util.Date;
+
+public class UsoEmpleado {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
+
+class Empleado {
+	
+	// Constructor
+	public void Empleado(String nom, double sue, int agno, int mes, int dia) {
+		
+	}
+	
+	private String nombre;
+	private double sueldo;
+	private Date altaContrato;
+}
