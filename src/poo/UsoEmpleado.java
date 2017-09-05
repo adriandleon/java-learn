1e89bce904c3dc769ff730ae846904daa2a2bde9
Adrian De León <adriandleon@gmail.com>
2017-09-05 08:39:42 -0400

    Sobrecarga de constructores
    Video 39

--- src/poo/UsoEmpleado.java
+++ src/poo/UsoEmpleado.java
@@ -7,13 +7,15 @@ public class UsoEmpleado {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Empleado[] misEmpleados = new Empleado[3];
+		Empleado[] misEmpleados = new Empleado[4];
 		
 		misEmpleados[0] = new Empleado("Paco Gomez", 85000, 1990, 12, 17);
 		
 		misEmpleados[1] = new Empleado("Ana Lopez", 95000, 1995, 06, 02);
 		
 		misEmpleados[2] = new Empleado("Maria Martin", 105000, 2002, 03, 15);
+		
+		misEmpleados[3] = new Empleado("Anthony Fernandez");
 		
 		for (Empleado e: misEmpleados) {
 			e.subeSueldo(5);
@@ -36,6 +38,11 @@ class Empleado {
 		sueldo = sue;
 		GregorianCalendar fecha = new GregorianCalendar(agno, mes-1, dia);
 		altaContrato = fecha.getTime();
+	}
+	
+	// Sobrecarga del metodo constructor
+	public Empleado(String nombre) {
+		this(nombre, 30000, 2000, 01, 01);
 	}
 	
 	// getter nombre
