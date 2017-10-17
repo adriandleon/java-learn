f402460ea1b32b6cc0fbf8b7b23d9f51e6d147cb
Adrian De León <adriandleon@gmail.com>
2017-10-16 22:38:47 -0400

    Eventos II
    Video 66

--- src/graphics/TestEvents.java
+++ src/graphics/TestEvents.java
@@ -1,12 +1,12 @@
 package graphics;
 
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
 public class TestEvents {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		ButtonsFrame myFrame = new ButtonsFrame();
 		myFrame.setVisible(true);
@@ -26,21 +26,49 @@ class ButtonsFrame extends JFrame {
 	public ButtonsFrame() {
 		
 		setTitle("Buttons and Events");
-		
 		setBounds(700, 300, 500, 300);
-		
 		ButtonsPanel myPanel = new ButtonsPanel();
-		
 		add(myPanel);
 	}
 }
 
 
-class ButtonsPanel extends JPanel {
+class ButtonsPanel extends JPanel implements ActionListener {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -915460853539774958L;
 	
+	JButton blueButton = new JButton("Blue");
+	JButton yellowButton = new JButton("Yellow");
+	JButton redButton = new JButton("Red");
+	
+	public ButtonsPanel() {
+		
+		add(blueButton);
+		add(yellowButton);
+		add(redButton);
+		
+		blueButton.addActionListener(this);
+		yellowButton.addActionListener(this);
+		redButton.addActionListener(this);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		Object buttonPressed = e.getSource();
+		
+		if (buttonPressed == blueButton) {
+			setBackground(Color.BLUE);			
+		}
+		else if (buttonPressed == yellowButton) {
+			setBackground(Color.YELLOW);
+		}
+		else if (buttonPressed == redButton){
+			setBackground(Color.RED);
+		}
+
+	}
 }
