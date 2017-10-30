890f1b96e26f9ec84530f03df9e45eb0ec4882cd
Adrian De León <adriandleon@gmail.com>
2017-10-30 15:08:44 -0400

    Events IX. Mouse Events II

--- src/graphics/MouseEvents.java
+++ src/graphics/MouseEvents.java
@@ -1,7 +1,6 @@
 package graphics;
 
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.event.*;
 import javax.swing.JFrame;
 
 public class MouseEvents {
@@ -20,38 +19,49 @@ class MouseFrame extends JFrame {
 	
 	public MouseFrame() {
 		
-		setBounds(700, 300, 600, 450);
+		setBounds(400, 300, 600, 450);
 		setVisible(true);
 		
 		addMouseListener(new MouseActions());
+		addMouseMotionListener(new MouseMotion());
 	}
 }
 
 
-class MouseActions implements MouseListener {
+class MouseActions extends MouseAdapter {
 
-	@Override
-	public void mouseClicked(MouseEvent arg0) {
-		System.out.println("Mouse clicked");
-	}
+//	@Override
+//	public void mouseClicked(MouseEvent e) {
+//		System.out.println("Coordinates: (" + e.getX() + ", " + e.getY() + ")");
+//
+//      System.out.println(e.getClickCount());
+//	}
 
-	@Override
-	public void mouseEntered(MouseEvent arg0) {
-		System.out.println("Mouse has entered");
-	}
+    @Override
+    public void mousePressed(MouseEvent mouseEvent) {
+        super.mousePressed(mouseEvent);
 
-	@Override
-	public void mouseExited(MouseEvent arg0) {
-		System.out.println("Mouse has exit");
-	}
+        if (mouseEvent.getModifiersEx() == MouseEvent.BUTTON1_DOWN_MASK) {
+            System.out.println("You pressed the left button");
+        }
+        else if (mouseEvent.getModifiersEx() == MouseEvent.BUTTON2_DOWN_MASK) {
+            System.out.println("You pressed the mouse wheel");
+        }
+        else if (mouseEvent.getModifiersEx() == MouseEvent.BUTTON3_DOWN_MASK) {
+            System.out.println("You pressed the right button");
+        }
+    }
+}
 
-	@Override
-	public void mousePressed(MouseEvent arg0) {
-		System.out.println("Mouse has been pressed");
-	}
+class MouseMotion implements MouseMotionListener {
 
-	@Override
-	public void mouseReleased(MouseEvent arg0) {
-		System.out.println("Mouse has been released");
-	}	
+    @Override
+    public void mouseDragged(MouseEvent mouseEvent) {
+        System.out.println("Mouse dragging");
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent mouseEvent) {
+        System.out.println("Mouse moving");
+    }
 }
