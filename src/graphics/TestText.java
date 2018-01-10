b8b7688854e2af65119c1989a6f1905cd9469f74
Adrian De León <adriandleon@gmail.com>
2018-01-10 09:01:10 -0400

    Swing Components. TextFields II

--- src/graphics/TestText.java
+++ src/graphics/TestText.java
@@ -1,6 +1,7 @@
 package graphics;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -29,26 +30,54 @@ class TextFrame extends JFrame {
 class TextPanel extends JPanel {
 
     private JTextField mField1;
+    private JLabel mResultLabel;
 
     TextPanel() {
 
+        setLayout(new BorderLayout());
+
+        JPanel topPanel = new JPanel();
+        topPanel.setLayout(new FlowLayout());
+
         JLabel label1 = new JLabel("Enter email address:");
-        add(label1);
+        topPanel.add(label1);
 
         mField1 = new JTextField(12);
-        add(mField1);
+        topPanel.add(mField1);
 
         JButton myButton = new JButton("Check");
         GetText myEvent = new GetText();
         myButton.addActionListener(myEvent);
-        add(myButton);
+        topPanel.add(myButton);
+
+        mResultLabel = new JLabel("", SwingConstants.CENTER);
+        add(mResultLabel, BorderLayout.CENTER);
+
+        add(topPanel, BorderLayout.NORTH);
     }
 
     private class GetText implements ActionListener {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            System.out.println(mField1.getText().trim());
+
+            boolean valid = checkEmail(mField1.getText().trim());
+
+            if (valid)
+                mResultLabel.setText("Correct");
+            else
+                mResultLabel.setText("Incorrect");
+        }
+
+        private boolean checkEmail(String email) {
+
+            int correct = 0;
+
+            for (int i = 0; i < email.length(); i++) {
+                if (email.charAt(i) == '@') correct++;
+            }
+
+            return correct == 1;
         }
     }
 }
