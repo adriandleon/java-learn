88624a66af318047d708df0f616ddbdbd51f9185
Adrian De León <adriandleon@gmail.com>
2018-08-08 08:57:09 -0400

    Practical Exercise II

--- src/exercises/DialogsFrame.java
+++ src/exercises/DialogsFrame.java
@@ -5,6 +5,8 @@ import java.awt.*;
 
 public class DialogsFrame extends JFrame {
 
+    private DialogsPanel typePanel;
+
     public DialogsFrame() {
         setTitle("Dialogs Test");
         setBounds(500, 300, 600, 450);
@@ -12,6 +14,11 @@ public class DialogsFrame extends JFrame {
         JPanel gridPanel = new JPanel();
         gridPanel.setLayout(new GridLayout(2, 3));
 
+        String typeOptions[] = {"Message", "Confirm", "Option", "Input"};
+
+        typePanel = new DialogsPanel("Type", typeOptions);
+
+        gridPanel.add(typePanel);
         add(gridPanel);
     }
 }
--- src/exercises/DialogsPanel.java
+++ src/exercises/DialogsPanel.java
@@ -1,4 +1,21 @@
 package exercises;
 
-public class DialogsPanel {
+import javax.swing.*;
+
+public class DialogsPanel extends JPanel {
+
+    public DialogsPanel(String title, String[] options) {
+        setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), title));
+
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        ButtonGroup group = new ButtonGroup();
+
+        for (int i = 0; i < options.length; i++) {
+            JRadioButton button = new JRadioButton(options[i]);
+            add(button);
+            group.add(button);
+            button.setSelected(i == 0);
+        }
+    }
 }
