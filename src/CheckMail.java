c2c7e50f476bc60e3f44144c6c06b85cd3053861
Adrian De León <adriandleon@gmail.com>
2018-11-29 08:56:55 -0400

    Exceptions V. Throw clause

--- src/CheckMail.java
+++ src/CheckMail.java
@@ -1,28 +1,42 @@
 import javax.swing.*;
+import java.io.EOFException;
 
 public class CheckMail {
 
-	public static void main(String[] args) {
-		
-		int at = 0;
-		boolean dot = false;
-		String email = JOptionPane.showInputDialog("Enter email");
-		
-		for (int i=0; i < email.length(); i++) {
-			if (email.charAt(i) == '@') {
-				at++;
-			}
-			
-			if (email.charAt(i) == '.') {
-				dot = true;
-			}
-		}
-		
-		if (at == 1 && dot) {
-			System.out.println("It is correct");
-		}
-		else {
-			System.out.println("Invalid email address");
-		}
-	}
+    public static void main(String[] args) {
+
+        String email = JOptionPane.showInputDialog("Enter email");
+
+        try {
+            checkEmail(email);
+        } catch (EOFException e) {
+            System.out.println("The length of the email address is too short.");
+        }
+
+    }
+
+    private static void checkEmail(String email) throws EOFException {
+        int at = 0;
+        boolean dot = false;
+
+        if (email.length() <= 3) {
+            throw new EOFException();
+        }
+
+        for (int i = 0; i < email.length(); i++) {
+            if (email.charAt(i) == '@') {
+                at++;
+            }
+
+            if (email.charAt(i) == '.') {
+                dot = true;
+            }
+        }
+
+        if (at == 1 && dot) {
+            System.out.println("It is correct");
+        } else {
+            System.out.println("Invalid email address");
+        }
+    }
 }
--- src/exceptions/InputData.java
+++ src/exceptions/InputData.java
@@ -14,32 +14,34 @@ public class InputData {
         Scanner input = new Scanner(System.in);
 
         int selection = input.nextInt();
-        input.close();
+
 
         if (selection == 1) {
-            enterData();
+            try {
+                enterData();
+            } catch (InputMismatchException e) {
+                System.out.println("You have enter wrong data.");
+            }
+
         } else {
             System.out.println("Bye");
             System.exit(0);
         }
+
+        input.close();
     }
 
     private static void enterData() throws InputMismatchException {
 
         Scanner input = new Scanner(System.in);
 
-        try {
-            System.out.println("Please, enter your name");
-            String name = input.nextLine();
+        System.out.println("Please, enter your name");
+        String name = input.nextLine();
 
-            System.out.println("Please, enter your age");
-            int age = input.nextInt();
-            input.close();
-            System.out.println("Hello, " + name + ". Next year you'll be " + age + " years old.");
-
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+        System.out.println("Please, enter your age");
+        int age = input.nextInt();
+        input.close();
+        System.out.println("Hello, " + name + ". Next year you'll be " + age + " years old.");
 
         System.out.println("Program exit");
     }
